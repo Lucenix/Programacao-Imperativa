ed066695dd67de048b6e8e20894694a0859ef74a
Lucenix <andrelucenaferreira@gmail.com>
2021-03-01 23:48:23 +0000

    Added First Notes from C Programming Manual

---
+++ Estudo Manual/estudo1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MAX_MARK 20.0
+#define MIN_MARK 9.5
+
+main () 
+{
+    /*
+    int c;
+    c = getchar();
+    while (c != EOF)
+    {
+        putchar(c);
+        c = getchar();
+    }
+    */
+
+    /*de maneira a conservar espaço vertical*/
+
+    int c;
+    while ((c=getchar())!=EOF)
+    {
+        putchar(c);
+    }
+    
+}
