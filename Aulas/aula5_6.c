a6c45656af072790cfb433ccb56d9c9f82fa199d
Lucenix <andrelucenaferreira@gmail.com>
2021-03-09 14:30:41 +0000

    added aula5_6.c

---
+++ Aulas/aula5_6.c
@@ -0,0 +1,34 @@
+//
+// Created by andre on 09/03/2021.
+//
+
+#include <stdio.h>
+
+int sum(int pInt[10], int n);
+
+int main() {
+    int a[10] = {1,2,3,4,5,6,7,8,9,0};
+
+    int s;
+
+    int *b;
+
+    b = a;
+
+    s = sum (b,10);
+
+    printf("%d\n", *a);
+    printf("%d\n", *b);
+    printf("%d",s);
+
+    return 0;
+}
+
+int sum(int pInt[10], int n) {
+    int i,x;
+    for(i=x=0; i<n; i++) {
+        x += pInt[i];
+    }
+    return x;
+}
+
